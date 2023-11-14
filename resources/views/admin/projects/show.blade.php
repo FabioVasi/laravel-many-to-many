19d3a5bfa68b0528c19aa1e288eea3c3141bb284
FabioVasi <vasifabio96@gmail.com>
2023-11-14 17:38:23 +0100

    add show technology

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,20 +6,27 @@
 
 
 
-<div class="container text-start">
+<div class="container text-start py-5">
 
-<h1 class="text-muted text-uppercase py-4">Project n°{{$project->id}}</h1>
+<h1 class="text-muted text-uppercase pb-4">Project n°{{$project->id}}</h1>
 
     <div class="row">
-        <div class="col-4">
-            <div class="card shadow">
-            <img  class="card-img-top" src="{{asset($project->image)}}" alt="">
-                <div class="card-body">
-                    <h4 class="py-3">{{$project->title}}</h4>
+        <div class="col-6">
+            <div class="card shadow rounded-3">
+            <img  class="card-img-top rounded-top-3" src="{{asset($project->image)}}" alt="">
+                <div class="card-body bg-light rounded-3">
+                    <h3 class="py-3">{{$project->title}}</h3>
                     <h6>{{$project->slug}}</h6>
-                    <p><a href="{{$project->github}}">{{$project->github}}</a></p>
-                    <p><a href="{{$project->second_link}}">{{$project->second_link}}</a></p>
-                    <p>{{$project->type ? $project->type->name : 'No Type'}}</p>
+                    <p><a class="text-decoration-none" href="{{$project->github}}"><span class="text-black fw-medium">Git Hub link:</span> {{$project->github}}</a></p>
+                    <p><a class="text-decoration-none" href="{{$project->second_link}}"><span class="text-black fw-medium">Second link:</span> {{$project->second_link}}</a></p>
+                    <h6>Type: {{$project->type ? $project->type->name : 'Undefined'}}</h6>
+
+                    <ul class="list-unstyled">
+                        @foreach($project->technologies as $technology)
+                        <li class="fw-medium">Technology: {{$project->technology ? $project->technology->name : 'Undefined'}}</li>
+                        @endforeach
+                    </ul>
+                    
                     <p>{{$project->content}}</p>
                 </div>
             </div>
