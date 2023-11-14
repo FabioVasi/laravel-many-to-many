c0145239e37cdcc000c7c38a8b9204b2b309323a
FabioVasi <vasifabio96@gmail.com>
2023-11-14 15:55:18 +0100

    add tables relation between technology and Project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,6 +22,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProjectSeeder::class,
+            TypeSeeder::class,
+            TechnologySeeder::class
         ]);
     }
 }
