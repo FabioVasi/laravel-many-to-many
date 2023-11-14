2254e5c1ff029b6565d82bb472d220a5e40e0866
FabioVasi <vasifabio96@gmail.com>
2023-11-14 15:15:00 +0100

    seeding and migrate technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['html', 'css', 'bootstrap', 'js', 'debugging', 'vite', 'php', 'db', 'mysql', 'laravel'];
+
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Str::slug($new_technology->name);
+            $new_technology->save();
+        }
     }
 }
